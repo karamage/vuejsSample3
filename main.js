9d46c5b4c6d399b260f793641be143299b03d7e4
Masaaki Kakimoto <karamage.umauma@gmail.com>
2018-06-05 19:38:34 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    count: 0,
+    list: ['りんご','banana', 'strawberry'],
+    show: true,
+    isChild: true,
+    isActive: true,
+    textColor: 'red',
+    bgColor: 'lightgray',
+    classObject: {
+      child: true,
+      'is-active': false
+    },
+    styleObject: {
+      color: 'red',
+      backgroundColor: 'lightgray'
+    },
+    item: {
+      id: 1,
+      src: 'item1.png',
+      alt: '商品サムネイル',
+      width: 200,
+      height: 200
+    },
+    radius: 50,
+  },
+  methods: {
+    handleClick: function (event) {
+      alert(event.target)
+    },
+    increment: function() {
+      this.count += 1
+    }
+  }
+})
