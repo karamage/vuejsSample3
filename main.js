29d4c00ddbe3e62bae50de6f161e03ef1d7af73c
Masaaki Kakimoto <karamage.umauma@gmail.com>
2018-06-12 20:15:45 +0900

    修正

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ var app = new Vue({
     name: 'キマイラ',
     message: 'Hello Vue.js!',
     count: 0,
-    list: ['りんご','banana', 'strawberry'],
+    budget: 300,
+    limit: 2,
+    list: [
+      { id: 1, name: 'りんご', price: 100},
+      { id: 2, name: 'ばなな', price: 200},
+      { id: 3, name: 'いちご', price: 400},
+      { id: 4, name: 'おれんじ', price: 300},
+      { id: 5, name: 'めろん', price: 500},
+    ],
+    //list: ['りんご','banana', 'strawberry'],
     list2: [
       { id: 1, name: 'スライム', hp: 100 },
       { id: 2, name: 'ゴブリン', hp: 200 },
@@ -47,6 +56,14 @@ var app = new Vue({
     }, this)
   },
   computed: {
+    matched: function() {
+      return this.list.filter(function(el){
+        return el.price <= this.budget
+      }, this)
+    },
+    limited: function() {
+      return this.matched.slice(0, this.limit)
+    },
     // 算出プロパティhalfWidthを定義
     halfWidth: function() {
       return this.width / 2
